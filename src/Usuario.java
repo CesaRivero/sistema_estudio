5166ee1c1382ae053810e7601438e714a2786161
CesaRivero <104017115+CesaRivero@users.noreply.github.com>
2023-04-02 17:13:47 -0300

    Initial commit

---
+++ src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
